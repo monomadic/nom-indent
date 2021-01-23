8a743996975dc4135574140ab7aed6d96c2ff913
monomadic <deathdisco@protonmail.com>
2021-01-23 23:29:03 +1100

    tests passing

--- src/parse.rs
+++ src/parse.rs
@@ -1,16 +1,109 @@
 use crate::Span;
 use nom::{
     bytes::complete::take_while,
-    character::complete::{alphanumeric0, alphanumeric1, newline, one_of},
-    combinator::{cut, opt},
-    multi::{many0, many0_count, many1, many1_count},
+    character::complete::{newline, one_of},
+    combinator::opt,
+    multi::many0_count,
     sequence::tuple,
     IResult,
 };
 use rctree::Node;
 
-/// break multiple lines
-pub(crate) fn lines(i: Span) -> IResult<Span, Vec<Node<Span>>> {
+fn next_indent(lines: &Vec<(usize, Span)>) -> usize {
+    *lines.first().map(|(indent, _line)| indent).unwrap_or(&0)
+}
+
+fn take_children<'a>(lines: &mut Vec<(usize, Span<'a>)>, indent: usize) -> Vec<Node<Span<'a>>> {
+    let mut siblings = Vec::new();
+
+    // println!(">>>{:?}", (indent, next_indent(&lines), &lines));
+
+    // while next line is a sibling or child
+    while next_indent(&lines) > indent {
+        // pop next line
+        if let Some((child_indent, line)) = remove_first(lines) {
+            let mut node = Node::new(line);
+
+            println!(
+                "found child {:?}",
+                (
+                    &line.to_string(),
+                    next_indent(&lines),
+                    indent,
+                    child_indent,
+                    &lines
+                )
+            );
+
+            // take remaining children
+            while next_indent(&lines) > child_indent {
+                let children = take_children(lines, indent);
+                println!("children: {:?}", &children);
+
+                for child in children {
+                    node.append(child);
+                }
+            }
+
+            siblings.push(node);
+        }
+    }
+
+    siblings
+}
+
+pub(crate) fn indented_tree(i: Span) -> IResult<Span, Vec<Node<Span>>> {
+    let (rem, mut lines) = indented_lines(i)?;
+    let mut buffer = Vec::new();
+
+    while let Some((indent, line)) = remove_first(&mut lines) {
+        println!("root: {}", line);
+        let mut node = Node::new(line);
+        let children = take_children(&mut lines, indent);
+
+        for child in children {
+            println!("direct child: {}", child);
+            node.append(child);
+        }
+        buffer.push(node);
+    }
+
+    Ok((rem, buffer))
+}
+
+pub(crate) fn _indented_tree(i: Span) -> IResult<Span, Vec<Node<Span>>> {
+    let mut current_indent = 0;
+    let mut current_node = Node::new(Span::new_extra("", ""));
+    let mut output = Vec::new();
+
+    let (rem, lines) = indented_lines(i)?;
+
+    for (indent, line) in lines {
+        let node = Node::new(line);
+        println!("indent {:?}", (indent, current_indent));
+
+        if indent == 0 {
+            current_node = node.clone();
+            output.push(node);
+        } else {
+            current_node.append(node.clone());
+
+            current_node = node;
+
+            // if indent > current_indent {
+            //     current_node.append(node);
+            // } else {
+            //     current_node.append(node);
+            // }
+        }
+
+        current_indent = indent;
+    }
+
+    Ok((rem, output))
+}
+
+fn indented_lines(i: Span) -> IResult<Span, Vec<(usize, Span)>> {
     // note that we cannot use many0 here because nom does not allow
     // the possibility of empty matchers on the many0 combinator.
 
@@ -19,9 +112,11 @@ pub(crate) fn lines(i: Span) -> IResult<Span, Vec<Node<Span>>> {
 
     loop {
         let result = line(rem)?;
-        let (_indent, content) = result.1;
+        let line = result.1;
+
         rem = result.0;
-        output.push(Node::new(content));
+
+        output.push(line);
 
         // kill the loop when input is consumed
         if rem.to_string().is_empty() {
@@ -29,19 +124,7 @@ pub(crate) fn lines(i: Span) -> IResult<Span, Vec<Node<Span>>> {
         }
     }
 
-    // println!("returning {:?}", (rem, output.clone()));
-
     Ok((rem, output))
-
-    // many0(line)(i).map(|(r, lines)| {
-    //     (
-    //         r,
-    //         lines
-    //             .into_iter()
-    //             .map(|(_line_count, line_content)| line_content)
-    //             .collect(),
-    //     )
-    // })
 }
 
 /// take a single line in the format (indent, content) and chomp optional newline
@@ -70,46 +153,10 @@ fn count_indentation(i: Span) -> IResult<Span, usize> {
     many0_count(one_of(" \t"))(i)
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_lines() {
-        println!("-- {:?}", lines(Span::new_extra("", "")));
-        assert!(lines(Span::new_extra("", "")).is_ok());
-        assert!(lines(Span::new_extra("\n", "")).is_ok());
-        assert!(line(Span::new_extra(" ", "")).is_ok());
-        assert!(line(Span::new_extra(" \n", "")).is_ok());
+/// remove first element of a vec. warning: expensive
+fn remove_first<T>(vec: &mut Vec<T>) -> Option<T> {
+    if vec.is_empty() {
+        return None;
     }
-
-    #[test]
-    fn test_line() {
-        assert!(line(Span::new_extra("", "")).is_ok());
-        assert!(line(Span::new_extra("\n", "")).is_ok());
-        assert!(line(Span::new_extra(" ", "")).is_ok());
-        assert!(line(Span::new_extra(" \n", "")).is_ok());
-    }
-
-    #[test]
-    fn test_count_indentation() {
-        fn assert_indent(input: &str, indent: usize) {
-            assert_eq!(
-                count_indentation(Span::new_extra(input, "<assert_indent>"))
-                    .unwrap()
-                    .1,
-                indent
-            );
-        }
-
-        assert_indent("", 0);
-        assert_indent("\n", 0);
-        assert_indent("aa bb", 0);
-        assert_indent("\ta\tb", 1);
-        assert_indent("\t\t", 2);
-        assert_indent(" ", 1);
-        assert_indent("\t", 1);
-        assert_indent("\t ", 2);
-        assert_indent("\t \t", 3);
-    }
+    Some(vec.remove(0))
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,42 +1,52 @@
 use nom_indent::*;
 
-fn assert_number_of_lines(test: &str, lines: usize) {
-    match indent(test, "<assertion>") {
-        Ok(result) => assert_eq!(result.1.len(), lines),
-        Err(e) => panic!("statement failed to parse {}, {}", test, e),
-    }
-}
+// fn assert_number_of_lines(test: &str, lines: usize) {
+//     match indent(test, "<assertion>") {
+//         Ok(result) => {
+//             let mut count = 0;
+
+//             for child in result.1.children() {
+//                 count = count + 1;
+//             }
+
+//             assert_eq!(count, lines)
+//         }
+//         Err(e) => panic!("statement failed to parse {}, {}", test, e),
+//     }
+// }
 
 /// a small render syntax for simple testing
 fn assert_mini_parser(input: &str, output: &str) {
     use rctree::Node;
 
-    fn bracketer(node: Node<Span>, buffer: &mut String) {
-        buffer.push_str(&node.borrow().to_string());
-    }
-
-    let mut buffer = String::new();
-
-    // parse
-    let result = indent(input, "<assertion>").expect("input failed to parse");
-    for node in result.1 {
+    fn bracketer(node: &Node<Span>, buffer: &mut String) {
         buffer.push_str("(");
-        bracketer(node, &mut buffer);
+        buffer.push_str(&node.borrow().to_string());
+        for child in node.children() {
+            bracketer(&child, buffer);
+        }
         buffer.push_str(")");
     }
 
-    println!("---{}", buffer);
+    // parse
+    let (_rem, lines) = indent(input, "<assertion>").expect("input failed to parse");
+
+    let mut buffer = String::new();
+    for line in lines {
+        bracketer(&line, &mut buffer);
+    }
+
+    assert_eq!(buffer, output);
 }
 
 #[test]
 fn test_parser() {
-    assert_number_of_lines("", 1);
-    assert_number_of_lines("\n", 1);
-    assert_number_of_lines("\n\n", 2);
-
-    assert_number_of_lines("a\nb\nc", 3);
-
     assert_mini_parser("", "()");
     assert_mini_parser("a", "(a)");
     assert_mini_parser("a\nb", "(a)(b)");
+    assert_mini_parser("a\n\tb", "(a(b))");
+    assert_mini_parser("a\n\tb\n\t\tc", "(a(b(c)))");
+    assert_mini_parser("a\n\tb\nc", "(a(b))(c)");
+    assert_mini_parser("a\n\tb\n\tc\nd", "(a(b)(c))(d)");
+    assert_mini_parser("a\n\tb\n\t\tc\n\td", "(a(b(c)(d)))");
 }
